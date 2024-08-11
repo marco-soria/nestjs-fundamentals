5f569b63bb1f4b8ea9c2d1dcd60fdc50ef39a9d1
marco-soria <marco.soriaa7@gmail.com>
2024-08-11 17:48:38 -0500

    put and delete

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Query, Post, Body, Put, Delete } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -33,5 +33,18 @@ export class ProductsController {
       message: 'accion de crear',
       payload,
     };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return id;
   }
   }
