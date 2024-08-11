cf3f0cb461bf9b4946e771003b352b42daae5051
marco-soria <marco.soriaa7@gmail.com>
2024-08-11 17:40:49 -0500

    post method

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,23 +1,37 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
     @Get()
     getProducts(
-      @Query('limit') limit = 100,
-      @Query('offset') offset = 0,
-      @Query('brand') brand: string,
-    ) {
-      return `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return {
+      message: `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
+    };
+  }
+
+  @Get('filter')
+  getProductFilter() {
+    return {
+        message:`yo soy un filter`
     }
-  
-    @Get('filter')
-    getProductFilter() {
-      return `yo soy un filter`;
+  }
+
+  @Get(':productId')
+  getOne(@Param('productId') productId: string) {
+    return {
+        message: `product ${productId}`
     }
-  
-    @Get(':productId')
-    getProduct(@Param('productId') productId: string) {
-      return `product ${productId}`;
-    }
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'accion de crear',
+      payload,
+    };
+  }
   }
